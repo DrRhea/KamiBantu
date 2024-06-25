bb91dd76092c20fd0d2f33e8518ec006a78a6d7c
yattaqiiii <yattaqifaza@example.com>
2024-06-26 00:12:42 +0700

    setup server bang

--- server/server.js
+++ server/server.js
@@ -1,15 +1,23 @@
 import express from "express";
 import cors from "cors";
-import records from "./routes/record.js";
+// import records from "./routes/record.js";
 
-const PORT = process.env.PORT || 5050;
+// const express = require('express');
+// const PORT = process.env.PORT || 5050;
 const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use("/record", records);
+// app.use("/record", records);
 
 // start the Express server
-app.listen(PORT, () => {
-  console.log(`Server listening on port ${PORT}`);
+// app.listen(PORT, () => {
+//   console.log(`Server listening on port ${PORT}`);
+// });
+
+app.get("/api", (req, res) => {
+  res.json({"users": ["userOne", "userTwo", "userThree"] });
 });
+
+
+app.listen(5000, () => {console.log("Server started on port 5000")});
