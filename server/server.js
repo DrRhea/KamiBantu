60c546965ca0c7fedb229a64204f930cf87e0cce
yattaqiiii <yattaqifaza@example.com>
2024-06-26 16:43:49 +0700

    add some changes on backend side

--- server/server.js
+++ server/server.js
@@ -1,10 +1,49 @@
 import express from "express";
+import fs from "fs";
 import cors from "cors";
 // import records from "./routes/record.js";
 
 // const express = require('express');
 // const PORT = process.env.PORT || 5050;
+// const { MongoClient} = require('mongodb');
+
+import { MongoClient } from "mongodb";
+
+const uri = 'mongodb://127.0.0.1:27017';
+const dbname = 'kitabantu';
+
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+async function main() {
+  try {
+    // Menghubungkan ke MongoDB
+    await client.connect();
+    console.log('Koneksi berhasil');
+    
+    // Mengambil database
+    const db = client.db(dbname);
+    
+    // Anda bisa mulai melakukan operasi pada database di sini
+    
+  } catch (error) {
+    console.error('Koneksi gagal:', error);
+  } finally {
+    // Menutup koneksi
+    await client.close();
+  }
+}
+
+main().catch(console.error);
+
+
+
 const app = express();
+const app1 = fs;
+
+// console.log(fs);
 
 app.use(cors());
 app.use(express.json());
@@ -15,9 +54,27 @@ app.use(express.json());
 //   console.log(`Server listening on port ${PORT}`);
 // });
 
-app.get("/api", (req, res) => {
+
+app.listen(5050, () => {console.log("Server started on port 5050")});
+
+app.get("/laporanak", (req, res) => {
   res.json({"users": ["userOne", "userTwo", "userThree"] });
 });
 
+app.get('/', (req, res) => {
+  res.send('mari bobo');
+});
 
-app.listen(5000, () => {console.log("Server started on port 5000")});
+app.get('/lapordewasa', (req, res) => {
+  res.send('mari makan');
+});
+
+
+app.get('/home', (req, res) => {
+  res.send('mari mandi');
+});
+
+app.get('/riwayat', (req, res) => {
+  res.send('mari solat');
+});
+
