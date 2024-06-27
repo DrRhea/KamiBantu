756377054c44a3bd396234a69cd10a20755853ff
DrRhea <czajr13@gmail.com>
2024-06-28 01:08:27 +0700

    fix some features

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Routes ,Route } from 'react-router-dom';
+import { Routes, Route, useLocation } from 'react-router-dom';
 import Home from './pages/Home';
 import Lapor from './pages/Lapor';
 import FormAnak from './pages/FormAnak'; 
@@ -7,13 +7,17 @@ import FormOrtu from './pages/FormOrtu';
 import Lacak from './pages/Lacak';
 import NotFound from './pages/NotFound';
 import Navbar from './Components/Navbar';
+import Footer from './Components/Footer';
 import Dashboard from './pages/Dashboard';
 
 
 const App = () => {
+  const hideNavbarPaths = ['/dashboard'];
   return (
     <>
-    <Navbar />
+      {!hideNavbarPaths.includes(location.pathname) && (
+        <Navbar />
+      )}
     <Routes>
       <Route path='/' element={<Home/>} />
       <Route path='/lapor' element={<Lapor/>} />
@@ -21,8 +25,9 @@ const App = () => {
       <Route path='/lapor/wali' element={<FormOrtu/>} />
       <Route path='/lacak' element={<Lacak/>} /> 
       <Route path='*' element={<NotFound />} />
-      <Route path='dashboard' element={<Dashboard />} />
+      <Route path='/dashboard' element={<Dashboard />} />
     </Routes>
+    <Footer />
     </>
   );
 };
